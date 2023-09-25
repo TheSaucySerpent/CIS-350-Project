ddef1c461c0fb86420a62a8624935dcbb16542c3
UJJW4LLL <145043454+UJJW4LLL@users.noreply.github.com>
2023-09-25 10:49:26 -0400

    Add files via upload

--- character.py
+++ character.py
@@ -1,10 +1,11 @@
 import pygame
+
 screen_width = 1200
 screen_height = 700
 
 
 class Character:
-    def __init__(self, x, y, width, height, speed, health, armor, gun):
+    def __init__(self, x, y, width, height, speed, health, armor, gun, image_path=None):
         self.x = x
         self.y = y
         self.width = width
@@ -16,6 +17,62 @@ class Character:
         self.last_hurt = 0
         self.last_dodge = 0
         self.invulnerable = False
+        self.image_counter = 0  # Initialize counter at 0
+        self.animation_delay = 250  # Time in milliseconds between animation frames
+        self.last_animation_update = 0  # Time of last animation frame update
+
+        if isinstance(image_path, str):  # Single image
+            self.image = pygame.image.load(image_path)
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        elif isinstance(image_path, dict):  # Animated sprites
+            self.images = {}
+            for direction, paths in image_path.items():
+                self.images[direction] = [pygame.image.load(p) for p in paths]
+                self.images[direction] = [pygame.transform.scale(img, (self.width, self.height)) for img in
+                                          self.images[direction]]
+            # Initialize with the first image in 'down' direction or any other default direction
+            self.image = self.images['down'][0]
+
+    def draw(self, screen):
+        if hasattr(self, 'image') and self.image:
+            screen.blit(self.image, (self.x, self.y))
+        else:
+            pygame.draw.rect(screen, (0, 0, 255), (self.x, self.y, self.width, self.height))
+
+    def move(self, keys, extra_speed, is_invulnerable=False):
+        new_x = self.x
+        new_y = self.y
+        current_direction = None  # Keep track of the current direction
+
+        if is_invulnerable:
+            self.invulnerable = True
+
+        if keys[pygame.K_w]:
+            new_y -= self.speed + extra_speed
+            current_direction = 'up'
+        if keys[pygame.K_a]:
+            new_x -= self.speed + extra_speed
+            current_direction = 'left'
+        if keys[pygame.K_s]:
+            new_y += self.speed + extra_speed
+            current_direction = 'down'
+        if keys[pygame.K_d]:
+            new_x += self.speed + extra_speed
+            current_direction = 'right'
+
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_animation_update >= self.animation_delay:
+            self.last_animation_update = current_time
+            if current_direction:
+                self.image_counter = (self.image_counter + 1) % len(self.images[current_direction])
+                self.image = self.images[current_direction][self.image_counter]
+
+        if 0 <= new_x <= screen_width - self.width and 0 <= new_y <= screen_height - self.height:
+            self.x = new_x
+            self.y = new_y
+
+        self.invulnerable = False
+
 
     def get_x(self):
         return self.x
@@ -23,28 +80,6 @@ class Character:
     def get_y(self):
         return self.y
 
-    def move(self, keys, extra_speed, is_invulnerable = False):
-        new_x = self.x
-        new_y = self.y
-
-        if is_invulnerable:
-            self.invulnerable = True
-
-        if keys[pygame.K_w]:
-            new_y -= self.speed + extra_speed
-        if keys[pygame.K_a]:
-            new_x -= self.speed + extra_speed
-        if keys[pygame.K_s]:
-            new_y += self.speed + extra_speed
-        if keys[pygame.K_d]:
-            new_x += self.speed + extra_speed
-
-        # Check if the new position is within the screen bounds
-        if 0 <= new_x <= screen_width - self.width and 0 <= new_y <= screen_height - self.height:
-            self.x = new_x
-            self.y = new_y
-
-        self.invulnerable = False
 
     def take_damage(self, damage):
         if not self.invulnerable:
@@ -71,6 +106,7 @@ class Character:
             self.health = maxhealth
         else:
             self.health += amount
+
 
 
 
--- glob_var.py
+++ glob_var.py
@@ -3,20 +3,27 @@ from weapons import Weapon
 from weapons import Shotgun
 from enemy import Enemy
 from object import Object
-
-#If there's a better way to do this please share this file looks goofy
-#If you want to add weapons, enemies and objects, add them to the corresponding list
+from room import Room
 
 
-#guns need an owner so this needs to be above it, but characters need a gun so it needs to be added after
-mc = Character(x=100, y=100, width=50, height=50, speed=.5, health=100, armor=50, gun=0)
+image_paths = {
+    'up': ['Up standing.png', 'Up running.png'],
+    'down': ['Down standing.png', 'Down running.png'],
+    'left': ['Left standing.png', 'Left running .png'],
+    'right': ['1.png', 'BackgroundEraser_image.png']
+}
 
-#(self, name, damage, proj_speed, attack_speed, mag_size, mag_count, reload_speed, owner)
-pistol = Weapon(name="Pistol", damage=10, proj_speed=.5, attack_speed=2, mag_size=9, mag_count=3, reload_speed=10, owner=mc)
-ar = Weapon(name="Assault Rifle", damage=15, proj_speed=1, attack_speed=5, mag_size=30, mag_count=2, reload_speed=20, owner=mc)
+mc = Character(x=1100, y=100, width=100, height=100, speed=1, health=100, armor=50, gun=0, image_path=image_paths)
+
+# (self, name, damage, proj_speed, attack_speed, mag_size, mag_count, reload_speed, owner)
+pistol = Weapon(name="Pistol", damage=10, proj_speed=.5, attack_speed=2, mag_size=9,
+                mag_count=3, reload_speed=10, owner=mc)
+ar = Weapon(name="Assault Rifle", damage=15, proj_speed=1, attack_speed=5, mag_size=30,
+            mag_count=2, reload_speed=20, owner=mc)
 dev_gun = Weapon("God's Draco", 100, 5, 50, 10000, 1, 10, mc)
 #shotgun adds spread and number of projectiles
-shotgun = Shotgun(name="Pump Shotgun", damage=20, proj_speed=1, attack_speed=2, mag_size=8, mag_count=2, reload_speed=15, owner=mc, spread=25, proj_number=8)
+shotgun = Shotgun(name="Pump Shotgun", damage=20, proj_speed=1, attack_speed=2, mag_size=8,
+                  mag_count=2, reload_speed=15, owner=mc, spread=25, proj_number=8)
 dev_shotgun = Shotgun("God's Sawed-Off", 50, 5, 10, 1000, 1, 10, mc, 20, 30)
 
 guns = [pistol, ar, dev_gun, shotgun, dev_shotgun]
@@ -25,13 +32,30 @@ guns = [pistol, ar, dev_gun, shotgun, dev_shotgun]
 mc.gun = dev_shotgun
 
 
-enemy1 = Enemy(x=700, y=200, width=50, height=50, speed=.2, health=50, armor=10, gun=pistol, character=mc)
-enemy2 = Enemy(x=500, y=500, width=50, height=50, speed=.25, health=50, armor=10, gun=pistol, character=mc)
-enemy3 = Enemy(x=700, y=500, width=50, height=50, speed=.3, health=50, armor=10, gun=pistol, character=mc)
+enemy1 = Enemy(x=700, y=200, width=50, height=50, speed=.1, health=50, armor=10, gun=pistol, character=mc)
+
+enemy12 = Enemy(20,34,34,34,0.5,99,88,7,mc,"green monster.png")
+enemy2 = Enemy(x=500, y=500, width=50, height=50, speed=.01, health=50, armor=10, gun=pistol, character=mc)
+enemy3 = Enemy(x=700, y=500, width=50, height=50, speed=.03, health=50, armor=10, gun=pistol, character=mc)
+
 enemies = [enemy1, enemy2, enemy3]
 
 
+table = Object(x=200, y=200, width=100, height=100, health=1000,image_path="table.png")
+door = Object(x=1075, y=280, width=200, height=200, health=1000,image_path="door.png")
+golden_key = Object(800,200,100,100,999,"golden key.png")
 
-obj = Object(x=200, y=200, width=50, height=50, health=1000)
-obj2 = Object(x=500, y=200, width=100, height=50, health=1000)
-objs = [obj, obj2]
+objs_1 = [table,door,golden_key]
+
+room1 = Room("sandpixel.jpg",screen_width=1200,screen_height=700)
+
+room1.add_enemy(enemy1)
+room1.add_object(door)
+room1.add_object(table)
+room1.add_object(golden_key)
+room1.add_enemy(enemy12)
+room1.add_enemy(enemy3)
+
+room2 = Room("columbus city.jpg",screen_width=1200,screen_height=700)
+room2.add_character(mc)
+room2.add_enemy(enemy2)
