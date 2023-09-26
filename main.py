f6ff400b3f2cc5010247b5f32a78db8fabaced43
TheSaucySerpent <sjburden2013@gmail.com>
2023-09-26 12:28:32 -0400

    New Game Changes
    changes we worked on at the library moving things to new files.

--- main.py
+++ main.py
@@ -1,118 +1,49 @@
 import pygame
 import UI
-import colors
-import glob_var
+import game_functions
 
-pygame.init()
 
-screen_width = 1200
-screen_height = 700
+def main():
+    # initialize pygame
+    pygame.init()
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+    # create the screen
+    screen_width = 1200
+    screen_height = 700
+    screen = pygame.display.set_mode((screen_width, screen_height))
 
-font = pygame.font.Font(None, 36)
+    # set font for text
+    font = pygame.font.Font(None, 36)
 
-# This is just to make it easier to read in the running loop.
-player = glob_var.player
-obj = glob_var.obj
-enemies = glob_var.enemies
+    # flags for running the program and the game itself
+    program_running = True
+    game_in_progress = False
 
-running = True
-game_in_progess = False
-game_over = False
+    game = game_functions.Game(screen, screen_width, screen_height, font)
 
-# display the title menu
-UI.display_menu(screen, screen_width, screen_height, font)
+    # display the title menu
+    UI.display_menu(screen, screen_width, screen_height, font)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    while program_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                program_running = False
+            # elif event.type == pygame.FULLSCREEN:
+            #     size = pygame.display.Info()
+            #     w,h = size.current_w,size.current_h
+            #     UI.display_menu(screen, w, h, font)
 
-        # to register key presses
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            elif event.key == pygame.K_SPACE:
-                # starts the game
-                game_in_progess = True
+            # to register key presses
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    program_running = False
+                elif event.key == pygame.K_n:
+                    # starts the game
+                    game_in_progress = True
 
-    if game_in_progess:
-        screen.fill(colors.BLACK)
-        # If r is press, calls weapon reload function
-        # If space is pressed and its been 1 second, does a dodge/teleport with invulnerability
-        # wasd to move
-        keys = pygame.key.get_pressed()
-        if not game_over:
-            if keys[pygame.K_r]:
-                player.gun.reload()
-            if keys[pygame.K_SPACE]:
-                current_time = pygame.time.get_ticks()
-                if current_time - player.last_dodge > 1000:
-                    player.move(keys, 200, True)
-                    player.last_dodge = current_time
-                else:
-                    player.move(keys, 0)
-            else:
-                player.move(keys, 0)
+        if game_in_progress:
+            game.run_game()
 
-            if pygame.mouse.get_pressed()[0]:  # Left mouse button
-                if player.gun != 0:
-                    player.gun.attack()
-                else:
-                    print("You don't got a gun!")
 
-        # Labeled separately so I can use pygame's .colliderect()
-        player_rect = (player.x, player.y, player.width, player.height)
-
-        # Draw the character
-        pygame.draw.rect(screen, (0, 0, 255), player_rect)
-
-        # Draw projectiles
-        for g in glob_var.guns:
-            for p in g.projectiles:
-                p.move()
-                pygame.draw.rect(screen, (255, 255, 0), (p.x, p.y, p.width, p.height))
-            g.update_projectiles()
-
-        # Draw and update enemy: makes enemies move towards player, checks for enemy-player collision and
-        # kills them
-        for enemy in enemies:
-            enemy.move_towards_character()
-
-            if enemy.health > 0:
-                pygame.draw.rect(screen, (255, 0, 0), (enemy.x, enemy.y, enemy.width, enemy.height))
-            else:
-                enemies.remove(enemy)
-
-        # Draw objects & handle object collision
-        for obj in glob_var.objs:
-            for entity in glob_var.entities:
-                entity_rect = pygame.Rect(entity.x, entity.y, entity.width, entity.height)
-                if entity_rect.colliderect(obj.obj_rect):
-                    # Left Border
-                    if entity.x + entity.width < obj.x + 2:
-                        entity.x -= 1
-                    # Right Border
-                    elif entity.x > obj.x + obj.width - 2:
-                        entity.x += 1
-                    # Upper Border
-                    elif entity.y < obj.y:
-                        entity.y -= 1
-                    # Lower Board
-                    elif entity.y > obj.y:
-                        entity.y += 1
-            pygame.draw.rect(screen, (192, 192, 192), obj.obj_rect)
-
-            UI.display_player_stats(screen, player, font)
-
-            # Death Message/Game Over
-            if player.health == 0:
-                ded_text = font.render(f"You Died.", True, (255, 0, 0))
-                screen.blit(ded_text,
-                            ((screen_width - ded_text.get_width()) // 2,
-                             (screen_height - ded_text.get_height()) // 2))
-                game_over = True
-
-        # update the display
-        pygame.display.update()
+if __name__ == '__main__':
+    main()
