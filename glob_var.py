15c98df38619ed5f5f049d161074be43e29c45c0
UJJW4LLL <145043454+UJJW4LLL@users.noreply.github.com>
2023-10-19 20:16:40 -0400

    created a maze

--- glob_var.py
+++ glob_var.py
@@ -16,7 +16,7 @@ image_paths = {
 }
 
 #guns need an owner so this needs to be above it, but characters need a gun so it needs to be added after
-player = Character(name="Player",x=1100, y=100, width=50, height=50, speed=1, health=100, armor=50, gun=0,image_path="images/white_square.png")
+player = Character(name="Player",x=100, y=500, width=50, height=50, speed=1, health=100, armor=50, gun=0,image_path="images/white_square.png")
 
 #(self, name, damage, proj_speed, attack_speed, mag_size, mag_count, reload_speed, owner)
 pistol = Weapon(name="Pistol", damage=10, proj_speed=.5, attack_speed=2, mag_size=9, mag_count=3, reload_speed=10, owner=player)
@@ -43,6 +43,8 @@ enemies = [enemy1, enemy2, enemy3, enemy4, enemy5]
 #Separate list including all enemies and main character, used for object collision
 entities = [player]
 for i in enemies:
+    i.x = 100
+    i.y = 50
     entities.append(i)
 
 
@@ -51,7 +53,34 @@ obj = Object(x=200, y=150, width=50, height=200, health=1000)
 obj2 = Object(x=200, y=150, width=700, height=50, health=1000)
 obj3 = Object(x=850, y=150, width=50, height=200, health=1000)
 obj4 = Object(x=200, y=150, width=300, height=200, health=1000)
-objs = [obj, obj2, obj3]
+
+
+top = Object(200,0,1000,50,999)
+bottom = Object(200,650,1000,50,999)
+front_down = Object(200,400,50,1000,999)
+front_up = Object(200,0,50,300,999)
+
+inside_1_1 = Object(350,130,50,450,999)
+inside_2_1 = Object(350,130,200,50,999)
+inside_2_2 = Object(500,0,50,450,999)
+inside_2_3 = Object(500,525,50,450,999)
+inside_3_1 = Object(650,200,50,380,999)
+inside_3_2 = Object(650,0,50,130,999)
+
+inside_4_1 = Object(700,530,250,50,999)
+inside_4_2 = Object(800,0,50,450,999)
+inside_4_3 = Object(950,130,50,450,999)
+
+inside_5_1 = Object(1080,400,50,450,999)
+inside_5_2 = Object(950,400,150,50,999)
+inside_5_3 = Object(1080,0,50,330,999)
+
+objs = [top,bottom,front_down,front_up,inside_1_1,inside_2_1,inside_2_2 , inside_2_3,inside_3_1,inside_3_2,inside_4_1,inside_4_2,inside_4_3,
+        inside_5_1,inside_5_2,inside_5_3]
+
+for i in objs:
+    i.image_path= "images/bush-removebg-preview.png"
+
 player2 = Character(name="mc",x=1100, y=100, width=10, height=100, speed=1, health=100, armor=50, gun=0,image_path="images/door.png")
 
 
@@ -67,6 +96,5 @@ for i in objs:
 door = Item(100,100,100,150,5000,"images/door.png")
 r1.add_item(door)
 door.bounce()
-
 
 
